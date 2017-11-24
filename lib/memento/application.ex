5e40e0e8845924b5157f82829770aae597bb5787
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-11-24 10:06:44 +0000

    Generates project

---
+++ lib/memento/application.ex
@@ -0,0 +1,20 @@
+defmodule Memento.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Memento.Worker.start_link(arg)
+      # {Memento.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Memento.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
