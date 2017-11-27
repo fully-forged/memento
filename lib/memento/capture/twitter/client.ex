2ae5f6ea0b4d0c5aee1412a47eac13082022073b
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-11-27 18:11:24 +0000

    Can fetch raw Twitter favs data

--- lib/memento/capture/twitter/client.ex
+++ lib/memento/capture/twitter/client.ex
@@ -19,6 +19,31 @@ defmodule Memento.Capture.Twitter.Client do
     end
   end
 
+  def get_favs(token, screen_name, since) do
+    headers = [{"Authorization", "Bearer #{token}"}]
+    url = "https://api.twitter.com/1.1/favorites/list.json"
+    params = qs_params(screen_name, since)
+
+    case HTTPClient.get(url, headers, params) do
+      %{status_code: 200, body: resp_body} ->
+        Poison.decode(resp_body)
+
+      error_response = %HTTPClient.ErrorResponse{} ->
+        {:error, error_response}
+
+      other_error ->
+        other_error
+    end
+  end
+
+  defp qs_params(screen_name, nil) do
+    %{"screen_name" => screen_name}
+  end
+
+  defp qs_params(screen_name, since) do
+    %{"screen_name" => screen_name, "since" => since}
+  end
+
   defp basic_auth(consumer_key, consumer_secret) do
     encoded = Base.encode64(consumer_key <> ":" <> consumer_secret)
     {"Authorization", "Basic " <> encoded}
