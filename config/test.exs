5e1556d877ee9ac45acb47815bc4820c1f6a3f48
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-12-01 14:48:56 +0000

    Sets up Gitlab CI

--- config/test.exs
+++ config/test.exs
@@ -4,8 +4,8 @@ config :logger, level: :error
 
 config :memento, Memento.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATA_DB_USER"),
-  password: System.get_env("DATA_DB_PASS"),
+  username: System.get_env("DATA_DB_USER") || "postgres",
+  password: System.get_env("DATA_DB_PASS") || "postgres",
   hostname: System.get_env("DATA_DB_HOST"),
   database: "memento_test",
   pool: Ecto.Adapters.SQL.Sandbox
