e7ed950255d15fedd3f40f8c0076b28e48326d8d
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-12-04 09:31:24 +0000

    More robust pagination logic
    Includes doctests.

--- lib/memento/api/pagination.ex
+++ lib/memento/api/pagination.ex
@@ -1,6 +1,28 @@
 defmodule Memento.API.Pagination do
   @default_per_page 20
 
+  @type limit :: non_neg_integer
+  @type offset :: non_neg_integer
+
+  @doc """
+  Extracts pagination values out of a query string params map (with string keys) and returns
+  a tuple with limit and offset, ready to be passet to an Ecto query.
+
+  Returns default values when keys are absent or their value cannot be parse to an int.
+
+  Expected keys are `page` (default 1) and `per_page` (default #{
+    @default_per_page
+  }).
+
+      iex> alias Memento.API.Pagination
+      iex> Pagination.parse(%{})
+      {20, 0}
+      iex> Pagination.parse(%{"page" => "2", "per_page" => 50})
+      {50, 50}
+      iex> Pagination.parse(%{"page" => "not-a-number", "per_page" => "50"})
+      {50, 0}
+  """
+  @spec parse(Map.t()) :: {limit, offset}
   def parse(qs_params) do
     page = parse(qs_params, "page", 1)
     per_page = parse(qs_params, "per_page", @default_per_page)
@@ -28,7 +50,7 @@ defmodule Memento.API.Pagination do
     end
   end
 
-  defp sanitize(v) when is_integer(v), do: v
+  defp sanitize(v) when is_integer(v), do: {:ok, v}
   defp sanitize(_v), do: :error
 
   defp ok_or_default({:ok, v}, _default), do: v
---
+++ test/memento/api/pagination_test.exs
@@ -0,0 +1,5 @@
+defmodule Memento.API.PaginationTest do
+  use ExUnit.Case, async: true
+
+  doctest Memento.API.Pagination
+end
