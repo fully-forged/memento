5e65abfe6c71b23fbbcf3ec812a1277f51928aa3
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-12-15 14:19:20 +0000

    Adds instapaper bookmarks to search index

---
+++ priv/repo/migrations/20171215133320_add_instapaper_bookmarks_to_entries_search_index.exs
@@ -0,0 +1,33 @@
+defmodule Memento.Repo.Migrations.AddInstapaperBookmarksToEntriesSearchIndex do
+  use Ecto.Migration
+
+  def up do
+    execute("""
+    DROP MATERIALIZED VIEW entries_search_index
+    """)
+
+    execute("""
+    CREATE MATERIALIZED VIEW entries_search_index AS
+    SELECT id,
+      CASE WHEN type='twitter_fav' THEN (content ->> 'screen_name') || (content ->> 'text')
+           WHEN type='github_star' THEN (content ->> 'owner') || (content ->> 'name') || (content ->> 'description')
+           WHEN type='pinboard_link' THEN (content ->> 'description')
+           WHEN type='instapaper_bookmark' THEN (content ->> 'title')
+           ELSE ''
+      END AS text
+    FROM entries
+    """)
+
+    execute("""
+    CREATE INDEX idx_flat_text
+    ON entries_search_index
+    USING gin(to_tsvector('english', text));
+    """)
+  end
+
+  def down do
+    execute("""
+    DROP MATERIALIZED VIEW entries_search_index
+    """)
+  end
+end
