d170e9980632cd7bdc85be2a0e519b382e2e3b61
Claudio Ortolina <claudio@fullyforged.com>
2018-05-12 16:46:03 +0100

    Disables Pinboard Feed in production
    Until removed from blacklist

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,11 @@
 use Mix.Config
 
-config :memento, assets_namespace: "prod"
+config :memento,
+  assets_namespace: "prod",
+  enabled_handlers: [
+    Memento.Capture.Twitter.Handler,
+    Memento.Capture.Github.Handler,
+    Memento.Capture.Instapaper.Handler
+  ]
 
 config :logger, level: :info
