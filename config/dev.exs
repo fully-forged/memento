07e9ebc67c75343d15d9362ea10642d4cb5fcc6b
Claudio Ortolina <claudio@pspdfkit.com>
2020-05-16 14:30:45 +0100

    Import static configuration with font support

--- config/dev.exs
+++ config/dev.exs
@@ -6,3 +6,19 @@ config :memento, Memento.Repo,
   database: "memento_dev",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
+
+config :memento, MementoWeb.Endpoint,
+  http: [port: 4000],
+  debug_errors: true,
+  code_reloader: true,
+  check_origin: false,
+  watchers: [
+    node: [
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
+
