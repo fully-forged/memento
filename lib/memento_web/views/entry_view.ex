e5007c5fb11e94eed22037438999dfbbab8d8bf0
Claudio Ortolina <claudio@pspdfkit.com>
2020-05-17 16:33:27 +0100

    Uniform render of all types

--- lib/memento_web/views/entry_view.ex
+++ lib/memento_web/views/entry_view.ex
@@ -4,24 +4,36 @@ defmodule MementoWeb.EntryView do
   def title(entry) do
     case entry.type do
       :github_star -> Map.get(entry.content, "name")
+      :twitter_fav -> Map.get(entry.content, "screen_name")
+      :pinboard_link -> Map.get(entry.content, "description")
+      :instapaper_bookmark -> Map.get(entry.content, "title")
     end
   end
 
   def description(entry) do
     case entry.type do
       :github_star -> Map.get(entry.content, "description")
+      :twitter_fav -> Map.get(entry.content, "text")
+      :pinboard_link -> Map.get(entry.content, "description")
+      :instapaper_bookmark -> Map.get(entry.content, "title")
     end
   end
 
-  def url(entry) do
+  def urls(entry) do
     case entry.type do
-      :github_star -> Map.get(entry.content, "url")
+      :github_star -> [Map.get(entry.content, "url")]
+      :twitter_fav -> Map.get(entry.content, "urls")
+      :pinboard_link -> [Map.get(entry.content, "href")]
+      :instapaper_bookmark -> [Map.get(entry.content, "url")]
     end
   end
 
   def icon(entry) do
     case entry.type do
       :github_star -> "icon-github"
+      :twitter_fav -> "icon-twitter"
+      :pinboard_link -> "icon-pushpin"
+      :instapaper_bookmark -> "icon-instapaper"
     end
   end
 
