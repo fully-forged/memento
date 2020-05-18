bae8bcb60dec04e833c3f1863931f3991fb51f81
Claudio Ortolina <claudio@pspdfkit.com>
2020-05-18 15:56:05 +0100

    Limit duration of WS connection

--- lib/memento_web/endpoint.ex
+++ lib/memento_web/endpoint.ex
@@ -11,11 +11,13 @@ defmodule MementoWeb.Endpoint do
   ]
 
   socket("/socket", MementoWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
   )
 
-  socket("/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]])
+  socket("/live", Phoenix.LiveView.Socket,
+    websocket: [timeout: 45_000, connect_info: [session: @session_options]]
+  )
 
   # Serve at "/" the static files from "priv/static" directory.
   #
