49dbdacf0025f74b90c830e829b9a966f5117085
Claudio Ortolina <claudio@pspdfkit.com>
2020-05-18 15:32:05 +0100

    Support environment database URL

--- lib/memento/repo.ex
+++ lib/memento/repo.ex
@@ -5,10 +5,16 @@ defmodule Memento.Repo do
 
   def init(_type, config) do
     config =
-      config
-      |> Keyword.put(:username, System.get_env("POSTGRES_USER"))
-      |> Keyword.put(:password, System.get_env("POSTGRES_PASSWORD"))
-      |> Keyword.put(:hostname, System.get_env("POSTGRES_HOSTNAME"))
+      case System.get_env("DATABASE_URL") do
+        nil ->
+          config
+          |> Keyword.put(:username, System.get_env("POSTGRES_USER"))
+          |> Keyword.put(:password, System.get_env("POSTGRES_PASSWORD"))
+          |> Keyword.put(:hostname, System.get_env("POSTGRES_HOSTNAME"))
+
+        url ->
+          Keyword.put(config, :url, url)
+      end
 
     {:ok, config}
   end
