b03dbb143f0c102443aabef6c2438170492e36ec
Claudio Ortolina <claudio@pspdfkit.com>
2020-06-24 10:35:30 +0100

    Add workaround for Pinboard BOM issue in response

--- lib/memento/capture/pinboard/client.ex
+++ lib/memento/capture/pinboard/client.ex
@@ -2,11 +2,16 @@ defmodule Memento.Capture.Pinboard.Client do
   @moduledoc false
   alias Memento.HTTPClient
 
+  @bom to_string([239, 187, 191])
+
   def get_links(token, since) do
     url = "https://api.pinboard.in/v1/posts/all"
     params = qs_params(token, since)
 
     case HTTPClient.get(url, [], params) do
+      %{status_code: 200, body: @bom <> resp_body} ->
+        Jason.decode(resp_body)
+
       %{status_code: 200, body: resp_body} ->
         Jason.decode(resp_body)
 
